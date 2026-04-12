0f93a8c8196502a22f06390d83dece06dd230f13
KrissyYIPPEE <124259710+KrissyYIPPEE@users.noreply.github.com>
2026-04-12 17:32:13 +0100

    Death Screen

--- Assets/Scripts/Player Character/ScreenOver.cs
+++ Assets/Scripts/Player Character/ScreenOver.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScreenOver : MonoBehaviour
 {
+    public string sceneName;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Retry()
     {
-        
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void Return()
+    {
+        // Assuming that the starting screen will always been scene 0
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 }
