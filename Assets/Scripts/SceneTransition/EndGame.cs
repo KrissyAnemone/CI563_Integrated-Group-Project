50e172defea435013cc2421afa9c88b0e81b46d8
ZSF13Uni <Z.Feast1@uni.brighton.ac.uk>
2026-05-13 16:05:36 +0100

    End Button Fix

--- Assets/Scripts/SceneTransition/EndGame.cs
+++ Assets/Scripts/SceneTransition/EndGame.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class EndGame : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void DoQuit()
     {
         Application.Quit();
